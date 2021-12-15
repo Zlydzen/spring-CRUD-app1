57c5624695c98b6435bb9d13a6879169651e0fd5
Mikita Byshniou <nick.byshnev@gmail.com>
2021-12-15 19:10:15 +0300

    initialize ArrayList people and add 4 new persons

--- src/main/java/by/byshnev/springmvc/dao/PersonDAO.java
+++ src/main/java/by/byshnev/springmvc/dao/PersonDAO.java
@@ -1,4 +1,20 @@
 package by.byshnev.springmvc.dao;
 
+import by.byshnev.springmvc.models.Person;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PersonDAO {
+    private List<Person> people;
+    private static int PEOPLE_COUNT;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Mikita Byshniou"));   //++PEOPLE_COUNT == 1;
+        people.add(new Person(++PEOPLE_COUNT,"Alan Alberneti"));     //++PEOPLE_COUNT == 2; etc.
+        people.add(new Person(++PEOPLE_COUNT,"Jhon Silver"));
+        people.add(new Person(++PEOPLE_COUNT,"Ken husband Barbie"));
+    }
+
 }
