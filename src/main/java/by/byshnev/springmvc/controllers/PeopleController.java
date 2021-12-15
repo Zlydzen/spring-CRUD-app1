7cfb35ae4d108eecb36f873eaadacfec34809192
Mikita Byshniou <nick.byshnev@gmail.com>
2021-12-16 00:05:15 +0300

    list of people in views -> index.html

--- src/main/java/by/byshnev/springmvc/controllers/PeopleController.java
+++ src/main/java/by/byshnev/springmvc/controllers/PeopleController.java
@@ -1,6 +1,7 @@
 package by.byshnev.springmvc.controllers;
 
 import by.byshnev.springmvc.dao.PersonDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,16 +12,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/people")
 public class PeopleController {
 
+    private PersonDAO personDAO;
+
+    @Autowired
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
     @GetMapping
     public String index(Model model){
         // получим всех людей из DAO и передадим на отображение в VIEW
-        return null;
+        model.addAttribute("people", personDAO.index());
+        return "people/index";
     }
 
     @GetMapping("/{id}")
     public String show(@RequestParam("id") int id, Model model){
         // получим одного человека по id из DAO и передадим на отображение в VEW
-        return null ;
+        model.addAttribute("person",personDAO.show(id));
+        return "people/show";
     }
 
 }
