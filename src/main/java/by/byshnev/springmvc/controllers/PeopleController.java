e9994e79ea7ac2220c6dd36e648761aa79cc5d2b
Mikita Byshniou <nick.byshnev@gmail.com>
2021-12-16 15:34:39 +0300

    chancge @GetParam to @PathVariable in PeopleController

--- src/main/java/by/byshnev/springmvc/controllers/PeopleController.java
+++ src/main/java/by/byshnev/springmvc/controllers/PeopleController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -27,7 +28,7 @@ public class PeopleController {
     }
 
     @GetMapping("/{id}")
-    public String show(@RequestParam("id") int id, Model model){
+    public String show(@PathVariable("id") int id, Model model){
         // получим одного человека по id из DAO и передадим на отображение в VEW
         model.addAttribute("person",personDAO.show(id));
         return "people/show";
