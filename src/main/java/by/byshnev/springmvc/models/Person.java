cf68f046d644613815d7c76c2b6c4da0e2400aeb
Mikita Byshniou <nick.byshnev@gmail.com>
2021-12-20 10:28:44 +0300

    add age and email fields in Person

--- src/main/java/by/byshnev/springmvc/models/Person.java
+++ src/main/java/by/byshnev/springmvc/models/Person.java
@@ -4,6 +4,8 @@ public class Person {
 
     private int id;
     private String name;
+    private int age;
+    private String email;
 
     public Person(){}
 
@@ -26,5 +28,21 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
